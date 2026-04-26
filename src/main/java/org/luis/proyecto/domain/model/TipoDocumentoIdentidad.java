db4ce8e550bb9b0e5835beeb69c5d3fd5540bd33
Fabiana <fabianafujino04@gmail.com>
2026-04-26 18:06:39 -0500

    probando 1

--- src/main/java/org/luis/proyecto/domain/model/TipoDocumentoIdentidad.java
+++ src/main/java/org/luis/proyecto/domain/model/TipoDocumentoIdentidad.java
@@ -3,4 +3,29 @@ package org.luis.proyecto.domain.model;
 public class TipoDocumentoIdentidad {
     private Integer id;
     private String nombre;
+
+    public TipoDocumentoIdentidad(Integer id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public TipoDocumentoIdentidad(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
