b97b3f863f655c99235e1e781d5f5600a546046b
JachiK2 <lmontenegrovargas5@gmail.com>
2026-04-30 11:39:07 -0500

    Luis

--- src/main/java/org/luis/proyecto/domain/model/TipoComprobantePago.java
+++ src/main/java/org/luis/proyecto/domain/model/TipoComprobantePago.java
@@ -4,4 +4,28 @@ public class TipoComprobantePago {
     private Integer id;
     private String tipoComprobante;
 
+    public TipoComprobantePago(Integer id, String tipoComprobante) {
+        this.id = id;
+        this.tipoComprobante = tipoComprobante;
+    }
+
+    public TipoComprobantePago(String tipoComprobante) {
+        this.tipoComprobante = tipoComprobante;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTipoComprobante() {
+        return tipoComprobante;
+    }
+
+    public void setTipoComprobante(String tipoComprobante) {
+        this.tipoComprobante = tipoComprobante;
+    }
 }
