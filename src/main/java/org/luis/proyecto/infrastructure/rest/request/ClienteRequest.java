7d495649103758756d76f173fea52df87523af53
Fabri <fabrizioayalaramirez@gmail.com>
2026-04-30 15:43:15 -0500

    .

--- src/main/java/org/luis/proyecto/infrastructure/rest/request/ClienteRequest.java
+++ src/main/java/org/luis/proyecto/infrastructure/rest/request/ClienteRequest.java
@@ -6,7 +6,6 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import org.luis.proyecto.domain.model.TipoDocumentoIdentidad;
 
-import java.math.BigDecimal;
 
 public record ClienteRequest(
         @NotNull
